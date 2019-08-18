4d8a7ad7a6d24a8e5221e5e6067320d0a03b2bda
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-18 23:01:54 +0700

    tugas_30_js
    tugas sudah ok !!!

---
+++ tugas-30.js
@@ -0,0 +1,6 @@
+function panggilRegexp(value){
+
+  var data = value.match(/[es]/g);
+  console.log(data);
+}
+panggilRegexp("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
